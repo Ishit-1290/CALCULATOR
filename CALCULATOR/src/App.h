76396e221d14630d1fef60beab9bbaf5abaa2bb7
Ishit-1290 <vinee042006@gmail.com>
2021-07-22 20:16:02 +0530

    Some new changes

--- CALCULATOR/src/App.h
+++ CALCULATOR/src/App.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "wx\wx.h"
+#include "MainWindow.h"
+class App : public wxApp
+{
+public:
+	App();
+	~App();
+	MainWindow* m_frame = nullptr;
+public:
+	virtual bool OnInit();
+};
