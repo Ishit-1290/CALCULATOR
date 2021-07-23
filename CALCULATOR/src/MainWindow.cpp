3dfc596033e36ebf45739a52129e11cf0f1b1364
Ishit-1290 <vinee042006@gmail.com>
2021-07-23 17:58:20 +0530

    Number grid done
    NUMBER GRID MADE with '=' button complementary of now.

--- CALCULATOR/src/MainWindow.cpp
+++ CALCULATOR/src/MainWindow.cpp
@@ -5,12 +5,12 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator") {
 	baseSizer = new wxBoxSizer(wxVERTICAL); //Vertical Sizer
 	calcArea = new wxTextCtrl(this, CALCAREA, "", wxPoint(0, 0), wxSize(100, 40));
 	btnGrid = new wxGridSizer(3, 3, wxSize(8,8));
-	
+	wxBoxSizer* horizontalSizer = new wxBoxSizer(wxHORIZONTAL);
 	// Making the Button Grid from 1 to 9
 	for (int i = 0, numbers = 7; i < 3; i++) {	
 
 		for (int j = 0; j < 3; j++) {
-			btnGrid->Add(new wxButton(this, 1000 + numbers, std::to_string(numbers), wxPoint(0, 0), wxSize(70, 50)));
+			btnGrid->Add(new wxButton(this, 1000 + numbers, std::to_string(numbers), wxPoint(0, 0), wxSize(78, 58)));
 			numbers++;
 		}
 		if (numbers == 10)
@@ -18,14 +18,20 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator") {
 		else
 			numbers = 1;
 	}
+	// Creating the last row
+	horizontalSizer->Add(new wxButton(this, 1000, "0", wxPoint(0, 0), wxSize(78, 58)),0, wxFIXED_MINSIZE | wxLEFT | wxTOP, 8);
+	horizontalSizer->Add(new wxButton(this, 1000, "=", wxPoint(0, 0), wxSize(164, 58)), 0, wxFIXED_MINSIZE | wxLEFT | wxTOP, 8);
+	
 
 	// Allowing calcArea to expand to full width
 	baseSizer->Add(calcArea, 0, wxEXPAND | wxALL, 8);
-	baseSizer->Add(btnGrid, 0, wxSHAPED | wxLEFT, 8);
-	baseSizer->Add(new wxButton(this, 1000, "0", wxPoint(0, 0), wxSize(66, 50)), 0, wxSHAPED | wxALL, 8);
+	// Adding the Button Grid with fixed size
+	baseSizer->Add(btnGrid, 0, wxFIXED_MINSIZE | wxLEFT, 8);
+	// Adding the last row
+	baseSizer->Add(horizontalSizer, 0, wxSHAPED);
 	
 	
-
+	
 	this->SetSizer(baseSizer);
 
 	//Relayout the window according to area acquired
